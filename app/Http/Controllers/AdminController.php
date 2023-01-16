343beb1fea6f4b9dd9990a68db0e34f6681f6a10
palindungan <rizkika.palindungan1@gmail.com>
2023-01-16 12:57:29 +0700

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -143,9 +143,7 @@ class AdminController extends AppBaseController
     {
         $phpWord = new \PhpOffice\PhpWord\PhpWord();
 
-
         $section = $phpWord->addSection();
-
 
         $description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,

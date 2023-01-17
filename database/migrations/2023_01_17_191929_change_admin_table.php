90885c0b2f407104a752b1707e1440e8e2832774
palindungan <rizkika.palindungan1@gmail.com>
2023-01-18 02:24:02 +0700

    Update 2023_01_17_191929_change_admin_table.php

--- database/migrations/2023_01_17_191929_change_admin_table.php
+++ database/migrations/2023_01_17_191929_change_admin_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::table('admin', function (Blueprint $table) {
-            $table->text('photo')->nullable();
-            $table->text('attachment')->nullable();
+            $table->string('photo')->nullable();
+            $table->string('attachment')->nullable();
         });
     }
 

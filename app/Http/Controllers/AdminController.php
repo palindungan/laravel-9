0bb163223953328a1dd0cb7dbf03ebe44365ccc5
palindungan <rizkika.palindungan1@gmail.com>
2023-01-18 02:12:17 +0700

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -145,12 +145,7 @@ class AdminController extends AppBaseController
 
         $section = $phpWord->addSection();
 
-        $description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        $description = "Lorem ipsum";
 
         $section->addImage("http://itsolutionstuff.com/frontTheme/images/logo.png");
         $section->addText($description);

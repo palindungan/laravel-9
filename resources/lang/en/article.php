ce4c13798a50d54d06703781000eccfe58ed7861
palindungan <rizkika.palindungan1@gmail.com>
2023-01-18 19:12:13 +0700

    lang

---
+++ resources/lang/en/article.php
@@ -0,0 +1,36 @@
+<?php
+return [
+    // ...
+
+    'unavailable_audits' => 'No Article Audits available',
+
+    'created'            => [
+        'metadata' => 'On :audit_created_at, :user_name [:audit_ip_address] created this record via :audit_url',
+        'modified' => [
+            'id'   => 'The id has been created : <strong>:new</strong>',
+            'name'   => 'The name has been created : <strong>:new</strong>',
+            'email' => 'The email has been created : <strong>:new</strong>',
+            'email_verified_at' => 'The email_verified_at has been created : <strong>:new</strong>',
+            'password' => 'The password has been created : <strong>:new</strong>',
+            'remember_token' => 'The remember_token has been created : <strong>:new</strong>',
+            'photo' => 'The photo has been created : <strong>:new</strong>',
+            'attachment' => 'The attachment has been created : <strong>:new</strong>',
+        ],
+    ],
+
+    'updated'            => [
+        'metadata' => 'On :audit_created_at, :user_name [:audit_ip_address] updated this record via :audit_url',
+        'modified' => [
+            'id'   => 'The id has been modified from <strong>:old</strong> to <strong>:new</strong>',
+            'name'   => 'The name has been modified from <strong>:old</strong> to <strong>:new</strong>',
+            'email' => 'The email has been modified from <strong>:old</strong> to <strong>:new</strong>',
+            'email_verified_at' => 'The email_verified_at has been modified from <strong>:old</strong> to <strong>:new</strong>',
+            'password' => 'The password has been modified from <strong>:old</strong> to <strong>:new</strong>',
+            'remember_token' => 'The remember_token has been modified from <strong>:old</strong> to <strong>:new</strong>',
+            'photo' => 'The photo has been modified from <strong>:old</strong> to <strong>:new</strong>',
+            'attachment' => 'The attachment has been modified from <strong>:old</strong> to <strong>:new</strong>',
+        ],
+    ],
+
+    // ...
+];

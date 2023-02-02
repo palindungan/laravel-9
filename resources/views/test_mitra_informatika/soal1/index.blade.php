26f36d0196dce7c82b4c3ab1b5d7625a7c557630
palindungan <rizkika.palindungan1@gmail.com>
2023-02-02 17:44:27 +0700

    Update index.blade.php

--- resources/views/test_mitra_informatika/soal1/index.blade.php
+++ resources/views/test_mitra_informatika/soal1/index.blade.php
@@ -31,11 +31,11 @@
 
             <div class="card-body">
                 <div class="row">
-                    <div class="form-group col-sm-6 mb-4">
+                    <div class="form-group col-sm-12 mb-4">
                         {!! Form::label('setting', 'Setting', ['class' => 'fw-bold form-label form-label-sm']) !!}
                         {!! Form::text('setting', null, ['class' => 'form-control form-control-solid form-control-sm', 'placeholder' => 'h,v,5']) !!}
-                        <span>mohon maaf yang negatif masih belum selesai</span>
                     </div>
+                    <span>mohon maaf yang negatif masih belum selesai</span>
                 </div>
             </div>
 

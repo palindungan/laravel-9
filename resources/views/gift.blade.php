6587dc0cc171809efa36fc50452ac48f27f2f052
palindungan <rizkika.palindungan1@gmail.com>
2023-04-09 22:17:42 +0700

    wes

--- resources/views/gift.blade.php
+++ resources/views/gift.blade.php
@@ -4,7 +4,11 @@
             <div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box" style="margin-bottom: 0px;">
                 <h2 style="">Hadiah</h2>
                 <p style="">Tanpa mengurangi rasa hormat, bagi keluarga, sahabat dan rekan yang ingin memberikan tanda kasih untuk kami, dapat melalui :</p>
+                <p><img style="width: 200px;" src="{{ asset('wedding/logos/Bank_Mandiri_logo_2016.svg') }}" alt="Logo Bank Mandiri"/></p>
+                <p><img src="{{ asset('wedding/images/qrcode_bank_mandiri_150.png') }}" alt="Rekening Bank Mandiri"/></p>
                 <p style="">a.n Salsabila Qutrotu'ain</p>
+                <p><span href="#" class="btn btn-info btn-sm">1430023634155</span></p>
+                <p><a href="#" class="btn btn-default btn-sm">Copy Rekening</a></p>
             </div>
         </div>
     </div>

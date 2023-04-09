0a247abc74f706e2c017fc27dd984aeebf15c7cf
palindungan <rizkika.palindungan1@gmail.com>
2023-04-09 16:59:06 +0700

    partials

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts_wedding.app')
 
 @section('content')
-    <h1>init</h1>
+    <header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(images/img_bg_2.jpg);"
+        data-stellar-background-ratio="0.5">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2 text-center">
+                    <div class="display-t">
+                        <div class="display-tc animate-box" data-animate-effect="fadeIn">
+                            <h1>Joefrey &amp; Sheila</h1>
+                            <h2>We Are Getting Married</h2>
+                            <div class="simply-countdown simply-countdown-one"></div>
+                            <p><a href="#" class="btn btn-default btn-sm">Save the date</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 @endsection
--- resources/views/layouts_wedding/app.blade.php
+++ resources/views/layouts_wedding/app.blade.php
@@ -11,118 +11,11 @@
     <div class="fh5co-loader"></div>
 
     <div id="page">
-        <nav class="fh5co-nav" role="navigation">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xs-2">
-                        <div id="fh5co-logo"><a href="index.html">Wedding<strong>.</strong></a></div>
-                    </div>
-                    <div class="col-xs-10 text-right menu-1">
-                        <ul>
-                            <li class="active"><a href="index.html">Home</a></li>
-                            <li><a href="about.html">Story</a></li>
-                            <li class="has-dropdown">
-                                <a href="services.html">Services</a>
-                                <ul class="dropdown">
-                                    <li><a href="#">Web Design</a></li>
-                                    <li><a href="#">eCommerce</a></li>
-                                    <li><a href="#">Branding</a></li>
-                                    <li><a href="#">API</a></li>
-                                </ul>
-                            </li>
-                            <li class="has-dropdown">
-                                <a href="gallery.html">Gallery</a>
-                                <ul class="dropdown">
-                                    <li><a href="#">HTML5</a></li>
-                                    <li><a href="#">CSS3</a></li>
-                                    <li><a href="#">Sass</a></li>
-                                    <li><a href="#">jQuery</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="contact.html">Contact</a></li>
-                        </ul>
-                    </div>
-                </div>
+        @include('layouts_wedding._partials.navigation')
 
-            </div>
-        </nav>
+        @yield('content')
 
-        <header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(images/img_bg_2.jpg);"
-            data-stellar-background-ratio="0.5">
-            <div class="overlay"></div>
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 col-md-offset-2 text-center">
-                        <div class="display-t">
-                            <div class="display-tc animate-box" data-animate-effect="fadeIn">
-                                <h1>Joefrey &amp; Sheila</h1>
-                                <h2>We Are Getting Married</h2>
-                                <div class="simply-countdown simply-countdown-one"></div>
-                                <p><a href="#" class="btn btn-default btn-sm">Save the date</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </header>
-
-        <div id="fh5co-started" class="fh5co-bg" style="background-image:url(images/img_bg_4.jpg);">
-            <div class="overlay"></div>
-            <div class="container">
-                <div class="row animate-box">
-                    <div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
-                        <h2>Are You Attending?</h2>
-                        <p>Please Fill-up the form to notify you that you're attending. Thanks.</p>
-                    </div>
-                </div>
-                <div class="row animate-box">
-                    <div class="col-md-10 col-md-offset-1">
-                        <form class="form-inline">
-                            <div class="col-md-4 col-sm-4">
-                                <div class="form-group">
-                                    <label for="name" class="sr-only">Name</label>
-                                    <input type="name" class="form-control" id="name" placeholder="Name">
-                                </div>
-                            </div>
-                            <div class="col-md-4 col-sm-4">
-                                <div class="form-group">
-                                    <label for="email" class="sr-only">Email</label>
-                                    <input type="email" class="form-control" id="email" placeholder="Email">
-                                </div>
-                            </div>
-                            <div class="col-md-4 col-sm-4">
-                                <button type="submit" class="btn btn-default btn-block">I am Attending</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <footer id="fh5co-footer" role="contentinfo">
-            <div class="container">
-
-                <div class="row copyright">
-                    <div class="col-md-12 text-center">
-                        <p>
-                            <small class="block">&copy; 2016 Free HTML5. All Rights Reserved.</small>
-                            <small class="block">Designed by <a href="http://freehtml5.co/"
-                                    target="_blank">FREEHTML5.co</a> Demo Images: <a href="http://unsplash.co/"
-                                    target="_blank">Unsplash</a></small>
-                        </p>
-                        <p>
-                        <ul class="fh5co-social-icons">
-                            <li><a href="#"><i class="icon-twitter"></i></a></li>
-                            <li><a href="#"><i class="icon-facebook"></i></a></li>
-                            <li><a href="#"><i class="icon-linkedin"></i></a></li>
-                            <li><a href="#"><i class="icon-dribbble"></i></a></li>
-                        </ul>
-                        </p>
-                    </div>
-                </div>
-
-            </div>
-        </footer>
+        @include('layouts_wedding._partials.footer')
     </div>
 
     <div class="gototop js-top">
@@ -130,6 +23,8 @@
     </div>
 
     @include('layouts_wedding._partials.scripts.main')
+    @stack('page_scripts')
+    @stack('third_party_scripts')
 </body>
 
 </html>

89c5fa900807d8fb8debcb42398e94ca1d4c758b
palindungan <rizkika.palindungan1@gmail.com>
2023-04-09 16:53:05 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

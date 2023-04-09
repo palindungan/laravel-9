b9095a22782781decabd82385276ffba57d849e3
palindungan <rizkika.palindungan1@gmail.com>
2023-04-09 16:45:11 +0700

    bk

--- resources/views/layouts_wedding/_partials/head.blade.php
+++ resources/views/layouts_wedding/_partials/head.blade.php
@@ -40,7 +40,10 @@
 
 <!-- Modernizr JS -->
 <script src="js/modernizr-2.6.2.min.js"></script>
+
 <!-- FOR IE9 below -->
-<!--[if lt IE 9]>
-	<script src="js/respond.min.js"></script>
-	<![endif]-->
+<!--
+	[if lt IE 9]>
+		<script src="js/respond.min.js"></script>
+	<![endif]
+-->

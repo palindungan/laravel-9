eb1f93c42c3544cd9e98f8c6abff572931f15db2
palindungan <rizkika.palindungan1@gmail.com>
2023-04-16 16:30:29 +0700

    migration 1

---
+++ database/migrations/2023_04_16_091818_change_admin_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('admin', function (Blueprint $table) {
+            $table->string('name_short');
+            $table->string('name_degree_first')->nullable();
+            $table->string('name_degree_last')->nullable();
+            $table->string('name_parent_male')->nullable();
+            $table->string('name_parent_female')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
---
+++ database/migrations/2023_04_16_092136_create_weddings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('weddings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('bride_id');
+            $table->integer('groom_id');
+            $table->integer('main_event_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('weddings');
+    }
+};

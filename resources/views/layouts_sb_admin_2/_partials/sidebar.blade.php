4e1304078c8008723356fd1348b0169627cac096
palindungan <rizkika.palindungan1@gmail.com>
2023-04-16 19:59:44 +0700

    table

--- resources/views/layouts_sb_admin_2/_partials/sidebar.blade.php
+++ resources/views/layouts_sb_admin_2/_partials/sidebar.blade.php
@@ -2,21 +2,22 @@
  <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon rotate-n-15">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="">
+        {{-- <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
-        </div>
-        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+        </div> --}}
+        <div class="sidebar-brand-text mx-3">Admin</div>
     </a>
 
     <!-- Divider -->
     <hr class="sidebar-divider my-0">
 
-    <!-- Nav Item - Dashboard -->
+    <!-- Nav Item - Administrator -->
     <li class="nav-item">
-        <a class="nav-link" href="index.html">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
+        <a class="nav-link" href="">
+            <i class="fas fa-fw fa-user"></i>
+            <span>Administrator</span>
+        </a>
     </li>
 
     <!-- Divider -->

7acfdf3f89c63a7c74fada4f990b1c4a7a186d1e
palindungan <rizkika.palindungan1@gmail.com>
2023-04-16 22:25:47 +0700

    Galeri Foto

--- Modules/Backend/Resources/views/photo_galleries/create.blade.php
+++ Modules/Backend/Resources/views/photo_galleries/create.blade.php
@@ -6,7 +6,7 @@
             <div class="row mb-2">
                 <div class="col-sm-12">
                     <h1>
-                    Create Photo Galleries
+                    Create Galeri Foto
                     </h1>
                 </div>
             </div>
--- Modules/Backend/Resources/views/photo_galleries/edit.blade.php
+++ Modules/Backend/Resources/views/photo_galleries/edit.blade.php
@@ -6,7 +6,7 @@
             <div class="row mb-2">
                 <div class="col-sm-12">
                     <h1>
-                        Edit Photo Gallery
+                        Edit Galeri Foto
                     </h1>
                 </div>
             </div>

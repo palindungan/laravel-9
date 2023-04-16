685faad8fcaac9ed2934793e5e47ab028afb315a
palindungan <rizkika.palindungan1@gmail.com>
2023-04-16 21:41:04 +0700

    Pernikahan

--- Modules/Backend/Resources/views/weddings/create.blade.php
+++ Modules/Backend/Resources/views/weddings/create.blade.php
@@ -6,7 +6,7 @@
             <div class="row mb-2">
                 <div class="col-sm-12">
                     <h1>
-                    Create Weddings
+                    Create Pernikahan
                     </h1>
                 </div>
             </div>

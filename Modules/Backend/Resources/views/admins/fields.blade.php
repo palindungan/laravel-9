de5b8a59534d0fc4febc8faecb9ed6082b564044
palindungan <rizkika.palindungan1@gmail.com>
2023-05-14 14:00:22 +0700

    field

--- Modules/Backend/Resources/views/admins/fields.blade.php
+++ Modules/Backend/Resources/views/admins/fields.blade.php
@@ -1,26 +1,26 @@
 <!-- Name Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('name', 'Name:') !!}
+    {!! Form::label('name', 'Nama Lengkap:') !!}
     {!! Form::text('name', null, ['class' => 'form-control', 'required', 'maxlength' => 255, 'maxlength' => 255]) !!}
 </div>
 
 <!-- Email Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('email', 'Email:') !!}
+    {!! Form::label('email', 'Alamat Email:') !!}
     {!! Form::email('email', null, ['class' => 'form-control', 'required', 'maxlength' => 255, 'maxlength' => 255]) !!}
 </div>
 
-<!-- Email Verified At Field -->
+{{-- <!-- Email Verified At Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('email_verified_at', 'Email Verified At:') !!}
     {!! Form::text('email_verified_at', null, ['class' => 'form-control','id'=>'email_verified_at']) !!}
-</div>
+</div> --}}
 
-@push('page_scripts')
+{{-- @push('page_scripts')
     <script type="text/javascript">
         $('#email_verified_at').datepicker()
     </script>
-@endpush
+@endpush --}}
 
 <!-- password Field -->
 <div class="form-group col-sm-6">
@@ -28,50 +28,56 @@
     {!! Form::password('password', ['class' => 'form-control', 'required', 'maxlength' => 255, 'maxlength' => 255]) !!}
 </div>
 
-<!-- Remember Token Field -->
+<!-- Konfirmasi Password Field -->
+<div class="form-group col-sm-6">
+    {!! Form::label('password_confirmation', 'Konfirmasi Password:') !!}
+    {!! Form::password('password_confirmation', ['class' => 'form-control', 'required', 'maxlength' => 255, 'maxlength' => 255]) !!}
+</div>
+
+{{-- <!-- Remember Token Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('remember_token', 'Remember Token:') !!}
     {!! Form::text('remember_token', null, ['class' => 'form-control', 'maxlength' => 100, 'maxlength' => 100]) !!}
-</div>
+</div> --}}
 
 <!-- Photo Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('photo', 'Photo:') !!}
+    {!! Form::label('photo', 'Foto:') !!}
     {!! Form::text('photo', null, ['class' => 'form-control', 'maxlength' => 255, 'maxlength' => 255]) !!}
 </div>
 
-<!-- Attachment Field -->
+{{-- <!-- Attachment Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('attachment', 'Attachment:') !!}
     {!! Form::text('attachment', null, ['class' => 'form-control', 'maxlength' => 255, 'maxlength' => 255]) !!}
-</div>
+</div> --}}
 
 <!-- Name Short Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('name_short', 'Name Short:') !!}
+    {!! Form::label('name_short', 'Nama Panggilan:') !!}
     {!! Form::text('name_short', null, ['class' => 'form-control', 'required', 'maxlength' => 255, 'maxlength' => 255]) !!}
 </div>
 
 <!-- Name Degree First Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('name_degree_first', 'Name Degree First:') !!}
+    {!! Form::label('name_degree_first', 'Gelar Awal:') !!}
     {!! Form::text('name_degree_first', null, ['class' => 'form-control', 'maxlength' => 255, 'maxlength' => 255]) !!}
 </div>
 
 <!-- Name Degree Last Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('name_degree_last', 'Name Degree Last:') !!}
+    {!! Form::label('name_degree_last', 'Gelar Akhir:') !!}
     {!! Form::text('name_degree_last', null, ['class' => 'form-control', 'maxlength' => 255, 'maxlength' => 255]) !!}
 </div>
 
 <!-- Name Parent Male Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('name_parent_male', 'Name Parent Male:') !!}
+    {!! Form::label('name_parent_male', 'Nama Ayah:') !!}
     {!! Form::text('name_parent_male', null, ['class' => 'form-control', 'maxlength' => 255, 'maxlength' => 255]) !!}
 </div>
 
 <!-- Name Parent Female Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('name_parent_female', 'Name Parent Female:') !!}
+    {!! Form::label('name_parent_female', 'Nama Ibu:') !!}
     {!! Form::text('name_parent_female', null, ['class' => 'form-control', 'maxlength' => 255, 'maxlength' => 255]) !!}
 </div>
--- Modules/Backend/Resources/views/admins/index.blade.php
+++ Modules/Backend/Resources/views/admins/index.blade.php
@@ -5,12 +5,12 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Mempelai</h1>
+                    <h1>Data Mempelai</h1>
                 </div>
                 <div class="col-sm-6">
                     <a class="btn btn-primary float-right"
                        href="{{ route('admins.create') }}">
-                        Add New
+                        Tambah Baru
                     </a>
                 </div>
             </div>

8aa5d7dee5ac7344b5e9ab7aa0a7164915861fd3
palindungan <rizkika.palindungan1@gmail.com>
2023-05-18 21:10:01 +0700

    Update 2023_04_16_092726_create_photo_galleries_table.php

--- database/migrations/2023_04_16_092726_create_photo_galleries_table.php
+++ database/migrations/2023_04_16_092726_create_photo_galleries_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('photo_galleries', function (Blueprint $table) {
             $table->id();
+            $table->integer('sort')->nullable();
             $table->string('photo');
             $table->timestamps();
         });

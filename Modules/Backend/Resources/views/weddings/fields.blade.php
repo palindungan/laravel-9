e47112862dafa2d7cd58937d89075ac64f246cfb
palindungan <rizkika.palindungan1@gmail.com>
2023-05-20 16:19:36 +0700

    Update fields.blade.php

--- Modules/Backend/Resources/views/weddings/fields.blade.php
+++ Modules/Backend/Resources/views/weddings/fields.blade.php
@@ -1,17 +1,17 @@
 <!-- Main Event Id Field -->
 <div class="form-group col-sm-12">
     {!! Form::label('main_event_id', 'Acara Utama:') !!}
-    {!! Form::select('main_event_id', $events, null, ['class' => 'form-control', 'required']) !!}
+    {!! Form::select('main_event_id', $events, null, ['class' => 'form-control select2', 'required']) !!}
 </div>
 
 <!-- Bride Id Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('bride_id', 'Pengantin Perempuan:') !!}
-    {!! Form::select('bride_id', $admins, null, ['class' => 'form-control', 'required']) !!}
+    {!! Form::select('bride_id', $admins, null, ['class' => 'form-control select2', 'required']) !!}
 </div>
 
 <!-- Groom Id Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('groom_id', 'Pengantin Pria:') !!}
-    {!! Form::select('groom_id', $admins, null, ['class' => 'form-control', 'required']) !!}
+    {!! Form::select('groom_id', $admins, null, ['class' => 'form-control select2', 'required']) !!}
 </div>

2a3929b9f25aa2ea6eebd07d178c4c96af16939f
palindungan <rizkika.palindungan1@gmail.com>
2023-05-20 23:08:33 +0700

    Update gift.blade.php

--- resources/views/home/gift.blade.php
+++ resources/views/home/gift.blade.php
@@ -2,13 +2,13 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box" style="margin-bottom: 0px;">
-                <h2 style="">Hadiah</h2>
-                <p style="">Tanpa mengurangi rasa hormat, bagi keluarga, sahabat dan rekan yang ingin memberikan tanda kasih untuk kami, dapat melalui :</p>
-                <p><img style="width: 200px;" src="{{ asset('wedding/logos/Bank_Mandiri_logo_2016.svg') }}" alt="Logo Bank Mandiri"/></p>
+                <h2 style="">{{ @$setting['6_1']->value }}</h2>
+                <p style="">{{ @$setting['6_2']->value }}</p>
+                <p><img style="width: 200px;" src="{{ asset('wedding/logos/Bank_Mandiri_logo_2016.svg') }}" alt="Logo Bank"/></p>
                 <p><img src="{{ asset('wedding/images/qrcode_bank_mandiri_150.png') }}" alt="Rekening Bank Mandiri"/></p>
-                <p style="">a.n Salsabila Qutrotu'ain</p>
+                <p style="">{{ @$setting['6_3']->value }}</p>
                 <p>
-                    <input type="text" class="btn btn-info btn-sm" readonly value="1430023634155">
+                    <input type="text" class="btn btn-info btn-sm" readonly value="{{ @$setting['6_4']->value }}">
                 </p>
                 <p>
                     <button class="btn btn-default btn-sm">Copy Rekening</button>

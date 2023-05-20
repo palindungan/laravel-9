a56bf84e863c3ea8becd9fcf1870f671f8560590
palindungan <rizkika.palindungan1@gmail.com>
2023-05-20 22:36:56 +0700

    Update couple.blade.php

--- resources/views/home/couple.blade.php
+++ resources/views/home/couple.blade.php
@@ -2,29 +2,28 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
-                <h2 style="font-size: 31px;">Assalamualaikum Warahmatullahi Wabarakatuh</h2>
-                {{-- <h3>November 28th, 2016 New York, USA</h3> --}}
-                <p>Dengan memohon rahmat dan ridho Allah SWT, kami mengundang Bapak/Ibu/Saudara/i untuk menghadiri resepsi pernikahan Putra-Putri Kami</p>
+                <h2 style="font-size: 31px;">{{ @$setting['2_1']->value }}</h2>
+                <p>{{ @$setting['2_2']->value }}</p>
             </div>
         </div>
         <div class="couple-wrap animate-box">
             <div class="couple-half">
                 <div class="groom">
-                    <img src="{{ asset('wedding-master/images/bride.jpg') }}" alt="groom" class="img-responsive">
+                    <img src="{{ @$bride->photo_thumbnail }}" alt="groom" class="img-responsive">
                 </div>
                 <div class="desc-groom">
-                    <h3>drh. Salsabila Qutrotu ain, S.KH</h3>
-                    <p>Putri dari Bapak Sigit Permono dan Ibu Cipuk Pangestu</p>
+                    <h3>{{ @$bride->name_degree_first ? @$bride->name_degree_first . ' ' : '' }}{{ @$bride->name }}{{ @$bride->name_degree_last }}</h3>
+                    <p>Putri dari Bapak {{ @$bride->name_parent_male }} dan Ibu {{ @$bride->name_parent_female }}</p>
                 </div>
             </div>
             <p class="heart text-center"><i class="icon-heart2"></i></p>
             <div class="couple-half">
                 <div class="bride">
-                    <img src="{{ asset('wedding-master/images/groom.jpg') }}" alt="groom" class="img-responsive">
+                    <img src="{{ @$groom->photo_thumbnail }}" alt="groom" class="img-responsive">
                 </div>
                 <div class="desc-bride">
-                    <h3>Rizkika Zakka Palindungan, S.Tr.Kom</h3>
-                    <p>Putra dari Bapak Sudardi dan Ibu Suheni</p>
+                    <h3>{{ @$groom->name_degree_first ? @$groom->name_degree_first . ' ' : '' }}{{ @$groom->name }}{{ @$groom->name_degree_last }}</h3>
+                    <p>Putra dari Bapak {{ @$groom->name_parent_male }} dan Ibu {{ @$groom->name_parent_female }}</p>
                 </div>
             </div>
         </div>

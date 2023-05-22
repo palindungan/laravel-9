51d7ea9346aebd1af13334540666ee9020254daf
palindungan <rizkika.palindungan1@gmail.com>
2023-05-22 20:19:33 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{code}', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/{code?}', [App\Http\Controllers\HomeController::class, 'index']);
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/backup', [App\Http\Controllers\HomeController::class, 'backup']);
 Route::get('/404', [App\Http\Controllers\HomeController::class, 'pageNotFound']);

79abf4cff5893f145cb41d42836b97ce81564861
palindungan <rizkika.palindungan1@gmail.com>
2023-05-29 23:40:33 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Events\PusherTest;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{code?}', [App\Http\Controllers\HomeController::class, 'index']);
-Route::post('/greeting-store', [App\Http\Controllers\HomeController::class, 'greetingStore']);
+Route::get('/linkstorage', function () {
+    Artisan::call('storage:link');
+});
 
 Route::get('/backup', [App\Http\Controllers\HomeController::class, 'backup']);
 Route::get('/404', [App\Http\Controllers\HomeController::class, 'pageNotFound']);
+
+Route::post('/greeting-store', [App\Http\Controllers\HomeController::class, 'greetingStore']);
+Route::get('/{code?}', [App\Http\Controllers\HomeController::class, 'index']);
